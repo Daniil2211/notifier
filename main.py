f4437683aa2fdfb6709de84e4bf1028b670cffad
Danila <ovufaddanila@mail.ru>
2022-02-10 18:40:21 +0300

    логика проверки начинался ли обзвон

--- main.py
+++ main.py
@@ -21,6 +21,10 @@ header = {'Authorization': "Bearer " + refresh_token,
 r = requests.get(url, headers=header)
 agents = r.json()
 
+is_begin_work = {}
+for agent in agents:
+    is_begin_work[agent['agent_uuid']] = False
+
 for agent in agents:
     url = 'https://api-v3.neuro.net/api/v2/ext/agent-settings/general?agent_uuid=' + agent['agent_uuid']
     header = {
@@ -38,25 +42,38 @@ for agent in agents:
     working_times = r.json()
 
     if working_times:
-        is_working_time = True
         for working_time in working_times:
-            if datetime.datetime.now().weekday() == working_time['day']:
+            if datetime.datetime.now(timezone(time_zone)).weekday() == working_time['day']:
                 time_now = datetime.datetime.now(timezone(time_zone)).strftime("%H:%M:%S")
                 if working_time['not_before'] <= time_now <= working_time['not_after']:
                     hour_offset = (datetime.datetime.now(timezone(time_zone)) - datetime.timedelta(hours=datetime.datetime.now().hour)).hour
-                    r = requests.post('https://api-v3.neuro.net/api/v2/ext/statistic/dialog-report',
-                                      json={"agent_uuid": agent['agent_uuid'],
-                                            "start": (datetime.datetime.now(timezone(time_zone)) - datetime.timedelta(minutes = 15, hours=(3 + hour_offset))).strftime("%Y-%m-%d %H:%M:%S"),
-                                            "end": (datetime.datetime.now(timezone(time_zone)) - datetime.timedelta(hours=(3 + hour_offset))).strftime("%Y-%m-%d %H:%M:%S")},
-                                      headers={
-                                            'Authorization': "Bearer " + refresh_token})
-                    total = r.json()['total']
-                    if total == 0:
-                        send_message(f"Обзвон {agent['name']} закончен!")
-                    else:
-                        #pass
-                        send_message(f"Обзвон {agent['name']} идет!")
-                    break
+                    if not is_begin_work[agent['agent_uuid']]:
+                        r = requests.post('https://api-v3.neuro.net/api/v2/ext/statistic/dialog-report',
+                                          json={"agent_uuid": agent['agent_uuid'],
+                                                "start": (working_time['not_before'] - datetime.timedelta(hours=(
+                                                            3 + hour_offset))).strftime("%Y-%m-%d %H:%M:%S"),
+                                                "end": (datetime.datetime.now(timezone(time_zone)) - datetime.timedelta(
+                                                    hours=(3 + hour_offset))).strftime("%Y-%m-%d %H:%M:%S")},
+                                          headers={
+                                              'Authorization': "Bearer " + refresh_token})
+                        total = r.json()['total']
+                        if total != 0:
+                            is_begin_work[agent['agent_uuid']] = True
+                    if is_begin_work[agent['agent_uuid']]:
+                        #hour_offset = (datetime.datetime.now(timezone(time_zone)) - datetime.timedelta(hours=datetime.datetime.now().hour)).hour
+                        r = requests.post('https://api-v3.neuro.net/api/v2/ext/statistic/dialog-report',
+                                          json={"agent_uuid": agent['agent_uuid'],
+                                                "start": (datetime.datetime.now(timezone(time_zone)) - datetime.timedelta(minutes=15, hours=(3 + hour_offset))).strftime("%Y-%m-%d %H:%M:%S"),
+                                                "end": (datetime.datetime.now(timezone(time_zone)) - datetime.timedelta(hours=(3 + hour_offset))).strftime("%Y-%m-%d %H:%M:%S")},
+                                          headers={
+                                                'Authorization': "Bearer " + refresh_token})
+                        total = r.json()['total']
+                        if total == 0:
+                            send_message(f"Обзвон {agent['name']} закончен!")
+                        else:
+                            #pass
+                            send_message(f"Обзвон {agent['name']} идет!")
+                        break
     else:
         #send_message(f"Робот {agent['name']} не работает")
         pass
